e3f1e761fd8c6533a79fc097911a8c3c1f4706e0
Gabriel Soledade <58832388+soledade2000@users.noreply.github.com>
2025-07-24 15:35:29 -0300

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+// serviceWorker.js
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('boitel-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './style.css',
+        './script.js',
+        './manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
